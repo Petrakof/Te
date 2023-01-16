2d10e89af7942bff1f42560e5d303d4758f5ecd9
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 10:37:25 +0300

    add

--- pages/1_📊_DataFrame.py
+++ pages/1_📊_DataFrame.py
@@ -4,16 +4,12 @@ from urllib.error import URLError
 from transformers import pipeline
 import plotly_express as px
 
-st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
+st.set_page_config(page_title="DataFrame", page_icon="📊")
 
 st.markdown("# DataFrame")
-st.sidebar.header("DataFrame)
+st.sidebar.header("DataFrame")
 st.write(
     """This demo shows how to use `st.write` to visualize Pandas DataFrames"""
 )
 
-@st.cache
-def get_UN_data():
-    AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-    df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-    return df.set_index("Region")
+
