d42c6a322f919eef91ed29e7823548b592b25428
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 20:42:16 +0300

    changes

--- pages/1_📊_DataFrame.py
+++ pages/1_📊_DataFrame.py
@@ -18,7 +18,7 @@ def read_data(uploaded_file):
 
 datafile = st.file_uploader("Загрузите файл csv", ["csv"])
 if datafile is None:
-    st.info("""Загрузите набор данных (.csv) на боковой панели, чтобы приступить к работе.""")
+    st.info("""Загрузите набор данных (.csv), чтобы приступить к работе.""")
     st.stop()
     
 data = read_data(datafile).copy()
