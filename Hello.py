74dbf982f957da34a900afe224f8e0f79ab7358d
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 21:24:43 +0300

    Changes

--- Hello.py
+++ Hello.py
@@ -19,3 +19,28 @@ display = Image.open('images/display.jpg')
 display = np.array(display)
 st.image(display)
 
+@st.cache
+def read_data(uploaded_file):
+    return pd.read_csv(uploaded_file)
+
+datafile = st.sidebar.file_uploader("Загрузите файл csv", ["csv"])
+if datafile is None:
+    st.info("""Загрузите набор данных (.csv) на боковой панели, чтобы приступить к работе.""")
+    st.stop()
+
+data = read_data(datafile).copy()
+
+model=pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+                      
+result = st.sidebar.button('Распознать')
+
+df_model = data.copy()
+
+if result:
+    lst = []
+    for i in df_model["text"]:
+        lst.append(model(str(i))[0]["label"])
+        df_model["Sentinent"]=pd.DataFrame(lst)
+
+st.write(df_model)
