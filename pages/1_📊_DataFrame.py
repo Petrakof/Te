e85c09e8c5143c38d15bd9cec657564b5ed9524e
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 12:18:22 +0300

    changes

--- pages/1_📊_DataFrame.py
+++ pages/1_📊_DataFrame.py
@@ -4,17 +4,14 @@ from urllib.error import URLError
 from transformers import pipeline
 import plotly.express as px
 
-st.set_page_config(page_title="DataFrame", page_icon="📊")
 
-st.sidebar.title("# DataFrame")
-
+st.markdown("# DataFrame")
+st.sidebar.markdown("# DataFrame")
 st.write(
     """This demo shows how to use `st.write` to visualize Pandas DataFrames"""
 )
 
 DATA = ('df.csv')
-
-
 @st.cache # для оптимизации работы приложения
 
 # Создадим функцию для загрузки данных
