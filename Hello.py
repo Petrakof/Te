4e4ba758a3d61fccb87a8460372fa4fedac8d24d
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 22:52:25 +0300

    Changes

--- Hello.py
+++ Hello.py
@@ -19,26 +19,26 @@ st.image(display)
 def mapping_demo():
     import streamlit as st
     import pandas as pd
-@st.cache
-def read_data(uploaded_file):
+    @st.cache
+    def read_data(uploaded_file):
     return pd.read_csv(uploaded_file)
 
-datafile = st.file_uploader("Загрузите файл csv", ["csv"])
-if datafile is None:
-    st.info("""Загрузите набор данных (.csv), чтобы приступить к работе.""")
-    st.stop()
+    datafile = st.file_uploader("Загрузите файл csv", ["csv"])
+    if datafile is None:
+        st.info("""Загрузите набор данных (.csv), чтобы приступить к работе.""")
+        st.stop()
 
-data = read_data(datafile).copy()
+    data = read_data(datafile).copy()
 
-model=pipeline("sentiment-analysis",   
+    model=pipeline("sentiment-analysis",   
                       "blanchefort/rubert-base-cased-sentiment")
                       
-result = st.sidebar.button('Распознать')
+    result = st.sidebar.button('Распознать')
 
-df_model = data.copy()
+    df_model = data.copy()
 
-if result:
-    lst = []
+    if result:
+        lst = []
     for i in df_model["text"]:
         lst.append(model(str(i))[0]["label"])
         df_model["Sentinent"]=pd.DataFrame(lst)
