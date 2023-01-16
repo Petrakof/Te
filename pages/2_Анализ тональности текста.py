e55caa23160a6569ccea19cbde506f25d39bfff4
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 13:17:44 +0300

    changes

--- pages/2_Анализ тональности текста.py
+++ pages/2_Анализ тональности текста.py
@@ -9,13 +9,19 @@ st.markdown("# Анализ тональности такста")
 st.sidebar.markdown( "Анализ тональности такста")
 
 @st.cache(allow_output_mutation=True)
+DATA = ('df.csv')
 
+# Создадим функцию для загрузки данных
 def load_model():
     model=pipeline("sentiment-analysis",   
                       "blanchefort/rubert-base-cased-sentiment")
     return model
 
 st.header ("Определение тональности текстов")
-st.subheader ("Введите текст для анализа")
-text = st.text_area(" ",height=100)
+
 result = st.button("Определить тональность текста")
+model = load_model()
+if result:
+    res = model(text)
+    sent = res[0]['label'] 
+    st.write(model(text)[0]["label"])
