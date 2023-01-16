183d33fd92869cf9ed004dcb4c6dd6ec62602cfc
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 13:21:54 +0300

    changes

--- pages/1_📊_DataFrame.py
+++ pages/1_📊_DataFrame.py
@@ -22,3 +22,14 @@ if show_data == True:
     st.subheader('Raw data')
     
     st.write(df)
+
+model=pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+
+df_model = df.copy()
+
+lst = []
+for i in df_model["text"]:
+  lst.append(model(str(i))[0]["label"])
+df_model["Sentinent"]=pd.DataFrame(lst)
+df_model
