97ab07c7d1d19a88a5702754f2a087a3859aeefd
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 11:37:35 +0300

    Changes

--- pages/1_📊_DataFrame.py
+++ pages/1_📊_DataFrame.py
@@ -11,5 +11,20 @@ st.sidebar.header("DataFrame")
 st.write(
     """This demo shows how to use `st.write` to visualize Pandas DataFrames"""
 )
+df = pd.read_csv("df.csv")
 
+@st.experimental_memo
+def convert_df(df):
+   return df.to_csv(index=False).encode('utf-8')
+
+
+csv = convert_df(df)
+
+st.download_button(
+   "Press to Download",
+   csv,
+   "file.csv",
+   "text/csv",
+   key='download-csv'
+)
 
