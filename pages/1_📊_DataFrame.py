7d92342eed9ea1697f2decb0f4bb98b64782484d
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 20:35:55 +0300

    changes

--- pages/1_📊_DataFrame.py
+++ pages/1_📊_DataFrame.py
@@ -21,4 +21,19 @@ if datafile is None:
     st.info("""Загрузите набор данных (.csv) на боковой панели, чтобы приступить к работе.""")
     st.stop()
     
-  
+  data = read_data(datafile).copy()
+
+model=pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+                      
+result = st.sidebar.button('Распознать')
+
+df_model = data.copy()
+
+if result:
+    lst = []
+    for i in df_model["text"]:
+        lst.append(model(str(i))[0]["label"])
+        df_model["Sentinent"]=pd.DataFrame(lst)
+
+st.write(df_model)
