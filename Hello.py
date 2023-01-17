50da1c495e1a6fdf5a2b83d1fe880348bc0b8afd
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-17 16:00:06 +0300

    changes

--- Hello.py
+++ Hello.py
@@ -86,13 +86,7 @@ def filter_demo():
         st.stop() 
 
     def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Adds a UI on top of a dataframe to let viewers filter columns
-    Args:
-        df (pd.DataFrame): Original dataframe
-    Returns:
-        pd.DataFrame: Filtered dataframe
-    """
+  
     modify = st.checkbox("Add filters")
 
     if not modify:
