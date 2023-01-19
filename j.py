bcc962ba81f37a48ba7fdfb99fef769d081abeff
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-19 18:13:48 +0300

    changes

--- j.py
+++ j.py
@@ -18,9 +18,6 @@ api_id=st.text_input ('Введите свой api_id: ', ' 29319788')
 api_hash = st.text_input ('Введите свой api_hash: ', ' a0c785ad0fd3e92e7c131f0a70987987')
 phone = st.text_input ("Введите свой номер телефона", "    ")
 
-client = TelegramClient(phone, api_id, api_hash)
-client.start()
-
 phone_2 = st.text_input ("Введите свой номер телефона", "    ")
 code= st.text_input ("Введите свой код", "    ")
 
