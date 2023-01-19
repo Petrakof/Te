2a1905c40bdc3761d243243080f2a9a91c6a4b7d
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-19 19:21:54 +0300

    changes

--- j.py
+++ j.py
@@ -16,16 +16,16 @@ from telethon.tl.types import PeerChannel
 import csv
 api_id=st.text_input ('Введите свой api_id: ',"29319788"  )
 api_hash = st.text_input ('Введите свой api_hash: ', 'a0c785ad0fd3e92e7c131f0a70987987')
-phone = st.text_input ("Введите свой номер телефона", "")
+
 
    
 async def main():
-    client = TelegramClient(phone, api_id, api_hash)
+    client = TelegramClient(" ", api_id, api_hash)
     await client.start()
 asyncio.run(main())
 
 
-
+phone = st.text_input ("Введите свой номер телефона", "")
 
 
 
