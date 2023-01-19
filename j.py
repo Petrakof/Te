34a2e3d40f76c48be2d04af9bb8af11d9c1f866b
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-19 20:18:29 +0300

    changes

--- j.py
+++ j.py
@@ -14,9 +14,6 @@ from telethon.tl.functions.messages import GetHistoryRequest
 from telethon.tl.types import PeerChannel
  
 import csv
-api_id=st.text_input ('Введите свой api_id: ',"29319788"  )
-api_hash = st.text_input ('Введите свой api_hash: ', 'a0c785ad0fd3e92e7c131f0a70987987')
-phone = st.text_input ("Введите свой номер телефона", "89112166840")
 
 
 async def main():
