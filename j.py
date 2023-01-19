9878bbcdd1e618314b2511131245a8b0b2dc1832
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-19 19:25:55 +0300

    changes

--- j.py
+++ j.py
@@ -17,10 +17,9 @@ import csv
 api_id=st.text_input ('Введите свой api_id: ',"29319788"  )
 api_hash = st.text_input ('Введите свой api_hash: ', 'a0c785ad0fd3e92e7c131f0a70987987')
 
-
    
 async def main():
-    client = TelegramClient(" ", api_id, api_hash)
+    client = TelegramClient('anon', api_id, api_hash)
     await client.start()
 asyncio.run(main())
 
