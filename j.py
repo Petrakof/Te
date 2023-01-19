38f1d819b04262cfa5724a5a89798ce78feb0560
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-19 20:30:07 +0300

    changes

--- j.py
+++ j.py
@@ -34,12 +34,4 @@ name=st.text_input ("Введите свой номер код", "Настя")
 
 chat=st.text_input ("Введите свой номер код", "t.me/+KxlX36pb-3hjMjRi")
 
-data = [] 
-
-with TelegramClient(name, api_id, api_hash) as client:
-    for message in client.iter_messages(chat, limit=100):
-        data.append([message.from_id.user_id, message.text])
-
-
-df = pd.DataFrame(data, columns=['user_id', 'text'])
 
