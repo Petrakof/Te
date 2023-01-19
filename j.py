1bd7ec375ad81c99909a4a1573bcf537e2055efa
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-19 19:12:42 +0300

    changes

--- j.py
+++ j.py
@@ -14,16 +14,15 @@ from telethon.tl.functions.messages import GetHistoryRequest
 from telethon.tl.types import PeerChannel
  
 import csv
-def start(): 
-    api_id=st.text_input ('Введите свой api_id: ',"29319788"  )
-    api_hash = st.text_input ('Введите свой api_hash: ', 'a0c785ad0fd3e92e7c131f0a70987987')
-    phone = st.text_input ("Введите свой номер телефона", "")
-    return 
-
+api_id=st.text_input ('Введите свой api_id: ',"29319788"  )
+api_hash = st.text_input ('Введите свой api_hash: ', 'a0c785ad0fd3e92e7c131f0a70987987')
+phone = st.text_input ("Введите свой номер телефона", "")
    
 async def main():
     client = TelegramClient(phone, api_id, api_hash)
     await client.start()
+asyncio.run(main())
+
 result = st.button('Распознать')
 
 name= st.text_input ("Введите свой ник в Телеграм", "Настя")
