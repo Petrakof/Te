da2888bf50b93436ef51cad251f60dc98b8c071e
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-21 17:49:48 +0300

    ch

--- Hello.py
+++ Hello.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from transformers import pipeline
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 from wordcloud import WordCloud
