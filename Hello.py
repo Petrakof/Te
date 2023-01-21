50203d56e261d349b14fe039a8566d3c51c8b88a
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-21 17:37:33 +0300

    changes

--- Hello.py
+++ Hello.py
@@ -18,6 +18,12 @@ datafile = st.sidebar.file_uploader("Загрузите файл csv", ["csv"])
 if datafile is None:
     st.info("""Загрузите набор данных (.csv) на боковой панели, чтобы приступить к работе.""")
     st.stop()
+progress_bar = st.progress(0)
+progress_text = st.empty()
+for i in range(101):
+    time.sleep(0.1)
+    progress_bar.progress(i)
+    progress_text.text(f"Progress: {i}%")
 
 data = read_data(datafile).copy()
 
@@ -26,12 +32,7 @@ model=pipeline("sentiment-analysis",
                       "blanchefort/rubert-base-cased-sentiment")
                       
 result = st.sidebar.button('🤗Распознать')
-progress_bar = st.progress(0)
-progress_text = st.empty()
-for i in range(101):
-    time.sleep(0.1)
-    progress_bar.progress(i)
-    progress_text.text(f"Progress: {i}%")
+
 
 df_model = data.copy()
 
